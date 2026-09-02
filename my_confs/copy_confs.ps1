235337819439feeed81386a8636cf1373b582a44
Jonas Ornfelt <jonasornfelt@outlook.com>
2026-09-02 09:13:05 +0200

    copy_confs.sh

--- my_confs/copy_confs.ps1
+++ my_confs/copy_confs.ps1
@@ -1,1 +1,2 @@
+rm *.conf
 cp $env:code_root_dir/Code2/C++/mangos-tbc/build/bin/x64_RelWithDebInfo/*.conf ./
---
+++ my_confs/linux/copy_confs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rm *.conf && cp "$HOME"/cmangos-tbc/run/etc/*.conf ./
